2dd21f58fb3ca94b23927105cd4eddecc7ab282f
danielweng0816 <50113105+danielweng0816@users.noreply.github.com>
2020-05-13 06:00:39 +0800

    Add files via upload

--- Daniel W - FSE.py
+++ Daniel W - FSE.py
@@ -1,6 +1,6 @@
 from pygame import *
 from random import *
-import math
+from math import *
 
 width,height=1200,800
 screen=display.set_mode((width,height))
@@ -15,9 +15,13 @@ YELLOW=(255,255,0)
 #initializing fonts
 font.init()
 comicFont=font.SysFont("Comic Sans MS",15)
+cointext=comicFont.render("Coins:",True,WHITE)
 
 
 #initializing variables
+coins=0
+score=0
+
 xpos=[]
 ypos=[]
 xspeed=0
@@ -25,12 +29,19 @@ yspeed=0
 gravity=0
 projectilex=0
 projectiley=0
+def dist(a,b,c,d):
+    return ((c-a)**2+(d-b)**2)**0.5
+hit = False
+release = False
+
+health=100
 
 projectiles = []
 
 count=0
 
-enemies = []
+enemiesx = []
+enemiesy = []
 
 shoot = False
 
@@ -52,10 +63,10 @@ while running:
                 click = True
             if evt.button == 3:
                 right_click = True
-            if evt.button == 4 and size <= 30:
-                size += 1
-            if evt.button == 5 and size >=1:
-                size -= 1
+            if evt.button == 4:
+                up=True
+            if evt.button == 5:
+                down=True
             if evt.type==MOUSEBUTTONUP:
                 screen.blit(screenCap,(0,0))
                 click=False
@@ -93,10 +104,13 @@ while running:
         title=comicFont.render("Katsura Kastle Defense",True,BLACK)
         screen.blit(title,(300,100))
         comicFont=font.SysFont("Comic Sans MS",25)
+        cointext=comicFont.render("Coins:",True,BLACK)
+        screen.blit(cointext,(900,30))
         playButton=comicFont.render("PLAY",True,WHITE)
         screen.blit(playButton,(920,310))
         shopButton=comicFont.render("SHOP",True,WHITE)
         screen.blit(shopButton,(920,410))
+
                 
 #Play-----------------------------------------------------------------------------------
 
@@ -107,8 +121,12 @@ while running:
         screen.blit(kastle,(0,80))
 
         #info
-        score=comicFont.render("SCORE:",True,WHITE)
-        screen.blit(score,(100,30))
+        scoretext=comicFont.render("SCORE:",True,WHITE)
+        currentscore=comicFont.render(str(score),True,WHITE)
+        screen.blit(scoretext,(100,30))
+        screen.blit(currentscore,(200,30))
+        cointext=comicFont.render("Coins:",True,WHITE)
+        screen.blit(cointext,(900,30))
 
         #Game--------------------------------------------------
 
@@ -119,36 +137,84 @@ while running:
         draw.rect(screen,BLACK,(48,710,22,25))
         draw.rect(screen,BLACK,(50,735,18,30))
         
-
-        #Shooting
-        
-        if mb[0] == 1 and shoot == False:
-            draw.line(screen,BLACK,(sx,sy),(mx,my),5)
-            shoot = True
+        #weapons
+        weapon="bow"
+        if weapon=="bow":
             
-        if shoot == True and mb[0] == 0 and sx!=mx and sy!=my:
-            if len(xpos)==0:
-                xpos.append(mx)
-                ypos.append(my)
-            h=abs(ypos[0]-sy)
-            l=sx-xpos[0]
-            gravity+=0.6
-            yspeed=yspeed+h/20
-            xspeed=xspeed+l/30
+        #Shooting
+            if mb[0] == 1 and shoot == False:
+                draw.line(screen,BLACK,(sx,sy),(mx,my),5)
+                pull=True
+            if pull==True and mb[0] == 0:
+                release=True
+            if pull==True and release==True:
+                shoot=True
+                pull=False
+            
+            if shoot == True and sx!=mx and sy!=my:
+                if len(xpos)<2:
+                    xpos.append(mx)
+                    ypos.append(my)
+                h=abs(ypos[0]-sy)
+                l=sx-xpos[0]
+                gravity+=0.65
+                yspeed=yspeed+h/20
+                xspeed=xspeed+l/30
 
-        projectilex=int(xspeed)+50
-        projectiley=int(700-yspeed+gravity**2)
-        draw.circle(screen,BLACK,(projectilex,projectiley),5)
 
-        if projectiley>800:
-            xspeed=0
-            yspeed=0
-            gravity=0
-            projectiley=0
-            xpos=[]
-            ypos=[]
-            shoot=False
-        print(shoot)
+            projectilex=int(xspeed)+50
+            projectiley=int(700-yspeed+gravity**2)
+            draw.circle(screen,BLACK,(projectilex,projectiley),5)
+
+            if projectiley>800:
+                xspeed=0
+                yspeed=0
+                gravity=0
+                projectiley=0
+                xpos=[]
+                ypos=[]
+                shoot=False
+                release=False
+
+        if weapon=="hello":
+            #shooting
+            if mb[0] == 1:
+                speed=100
+                xpos=mx
+                ypos=my
+                distx=abs(xpos-50)
+                disty=abs(700-ypos)
+                dist=(distx**2+disty**2)**0.5
+                xspeed=distx/dist*speed
+                yspeed=disty/dist*speed
+            projectilex=int(xspeed)+50
+            projectiley=int(700-yspeed)
+            draw.circle(screen,GREEN,(int(projectilex),int(projectiley)),5)
+
+            if projectiley>800 or projectiley<0 or projectilex>1000 or projectilex<0:
+                xspeed=0
+                yspeed=0
+                gravity=0
+                xpos=[]
+                ypos=[]
+                shoot=False
+
+            
+            
+                
+            
+            
+
+
+        #Health
+        draw.rect(screen,RED,(300,50,health,15))
+
+        if playerBody.collidepoint(mx,my) or playerFeet.collidepoint(mx,my):
+            health-=1
+
+        if health==0:
+            print("Game Over")
+            dead=True
         
                 
 
@@ -160,28 +226,45 @@ while running:
         
         
         #Enemies
-        if len(enemies)<5:
-            hello = (randint(200,1000),randint(100,700))
-            enemies.append(hello)
-        for i in range(len(enemies)):
-            draw.circle(screen,WHITE,enemies[i],50)
+        if len(enemiesx)<5:
+            enemiesx.append(randint(200,1000))
+            enemiesy.append(randint(100,700))
+        for i in range(len(enemiesx)):
+            if hit == False:
+                draw.circle(screen,WHITE,(enemiesx[i],enemiesy[i]),50)
+            if dist(projectilex,projectiley,enemiesx[i],enemiesy[i])<55:
+                hit=True
+            if hit == True:
+                coins+=1
+                score+=1
+                enemiesx.remove(enemiesx[i])
+                enemiesy.remove(enemiesy[i])
+                hit=False
+                break
+
+        #Enemies shooting
+        
         
         
         #-------------------------------------------------------
-        if mb[2] == 1:
-            dead = True
         if dead == True:
-            setting = 1
-            dead = False
+            screen.fill(0)
+            backToMenu=Rect(400,400,400,80)
+            draw.rect(screen,GREEN,(400,400,400,80))
+            if mb[0] == 1 and backToMenu.collidepoint(mx,my):
+                setting = 1
+                dead = False
+                score=0
+                health = 100
             
 #Shop------------------------------------------------------------------------------
 
     if setting == 3:
 
-        weaponItems = ["bow","samurai sword","a","b"]
-        armorItems = []
-        specialAbilityItems = []
-        statsItems = []
+        weaponItems = ["bow","a","b","c"]
+        armorItems = ["a","b","c","d"]
+        specialAbilityItems = ["a","b","c","d"]
+        statsItems = ["a","b","c","d"]
         
         back=Rect(0,0,100,50)
         weapons=Rect(205,140,190,50)
@@ -190,29 +273,34 @@ while running:
         stats=Rect(805,140,190,50)
         up=Rect(900,260,30,30)
         down=Rect(900,610,30,30)
-        selectionBar=Rect(900,300-count,30,300//len(weaponItems))
         border=Rect(250,250,700,400)
         bigBorder=Rect(200,200,800,500)
+    
 
         lis = [weapons,armor,special,stats]
 
         if mb[0]==1 and weapons.collidepoint(mx,my):
+            count=0
             shopSetting = 1
             draw.rect(screen,BLACK,(250,250,700,400))
         
-        if mb[0]==1 and armor.collidepoint(mx,my):
+        elif mb[0]==1 and armor.collidepoint(mx,my):
+            count=0
             shopSetting = 2
             draw.rect(screen,BLACK,(250,250,700,400))
             
-        if mb[0]==1 and special.collidepoint(mx,my):
+        elif mb[0]==1 and special.collidepoint(mx,my):
+            count=0
             shopSetting = 3
             draw.rect(screen,BLACK,(250,250,700,400))
             
-        if mb[0]==1 and stats.collidepoint(mx,my):
+        elif mb[0]==1 and stats.collidepoint(mx,my):
+            count=0
             shopSetting = 4
             draw.rect(screen,BLACK,(250,250,700,400))
 
         if shopSetting == 1:#Weapons section
+            selectionBar=Rect(900,300-count,30,300//len(weaponItems))
 
             #background
             background2 = image.load("images/Background2.png")
@@ -246,6 +334,8 @@ while running:
             
         if shopSetting == 2:#Armor section
 
+            selectionBar=Rect(900,300-count,30,300//len(armorItems))
+
             background3 = image.load("images/Background3.png")
             screen.blit(background3,(0,0))
             draw.rect(screen,GREEN,(0,0,100,50))
@@ -255,8 +345,26 @@ while running:
             draw.rect(screen,BLACK,(250,250,700,400))
             draw.rect(screen,BLACK,(250,250,700,400))
             draw.rect(screen,BLACK,(405,140,190,50),5)
+
+            draw.rect(screen,WHITE,(900,300-count,30,300//len(armorItems)))
+            
+            draw.rect(screen,WHITE,(900,260,30,30))
+            draw.rect(screen,WHITE,(900,610,30,30))
+
+            if mb[0] == 1 and up.collidepoint(mx,my) and selectionBar.collidepoint(910,300)==False:
+                selectionBar=Rect(900,300-count,30,300//len(armorItems))
+                count+=5
+            if mb[0] == 1 and down.collidepoint(mx,my) and selectionBar.collidepoint(910,600)==False:
+                selectionBar=Rect(900,300+count,30,300//len(armorItems))
+                count-=5
+
+            for i in range(3):
+                draw.rect(screen,WHITE,(300,275+120*i,500,100))
             
         if shopSetting == 3:#Special abilities section
+
+            selectionBar=Rect(900,300-count,30,300//len(specialAbilityItems))
+            
             background4 = image.load("images/Background4.jpg")
             screen.blit(background4,(0,0))
             draw.rect(screen,GREEN,(0,0,100,50))
@@ -266,8 +374,26 @@ while running:
             draw.rect(screen,BLACK,(250,250,700,400))
             draw.rect(screen,BLACK,(250,250,700,400))
             draw.rect(screen,BLACK,(605,140,190,50),5)
+
+            draw.rect(screen,WHITE,(900,300-count,30,300//len(specialAbilityItems)))
+            
+            draw.rect(screen,WHITE,(900,260,30,30))
+            draw.rect(screen,WHITE,(900,610,30,30))
+
+            if mb[0] == 1 and up.collidepoint(mx,my) and selectionBar.collidepoint(910,300)==False:
+                selectionBar=Rect(900,300-count,30,300//len(specialAbilityItems))
+                count+=5
+            if mb[0] == 1 and down.collidepoint(mx,my) and selectionBar.collidepoint(910,600)==False:
+                selectionBar=Rect(900,300+count,30,300//len(specialAbilityItems))
+                count-=5
+
+            for i in range(3):
+                draw.rect(screen,WHITE,(300,275+120*i,500,100))
             
         if shopSetting == 4:#stats section
+
+            selectionBar=Rect(900,300-count,30,300//len(statsItems))
+            
             background5 = image.load("images/Background5.jpg")
             screen.blit(background5,(0,0))
             draw.rect(screen,GREEN,(0,0,100,50))
@@ -277,6 +403,21 @@ while running:
             draw.rect(screen,BLACK,(250,250,700,400))
             draw.rect(screen,BLACK,(250,250,700,400))
             draw.rect(screen,BLACK,(805,140,190,50),5)
+
+            draw.rect(screen,WHITE,(900,300-count,30,300//len(statsItems)))
+            
+            draw.rect(screen,WHITE,(900,260,30,30))
+            draw.rect(screen,WHITE,(900,610,30,30))
+
+            if mb[0] == 1 and up.collidepoint(mx,my) and selectionBar.collidepoint(910,300)==False:
+                selectionBar=Rect(900,300-count,30,300//len(statsItems))
+                count+=5
+            if mb[0] == 1 and down.collidepoint(mx,my) and selectionBar.collidepoint(910,600)==False:
+                selectionBar=Rect(900,300+count,30,300//len(statsItems))
+                count-=5
+
+            for i in range(3):
+                draw.rect(screen,WHITE,(300,275+120*i,500,100))
 
         #Hovering mouse over stuff
         for i in range(4):
@@ -305,5 +446,3 @@ while running:
     display.flip()
             
 quit()
-
-
